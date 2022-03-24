ba128b9e415616445bc6fb8fa9f7ad2fff8e8c3e
Alabhya Jindal <alabhya10@gmail.com>
2022-03-24 15:24:29 +0530

    fcc exercise

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
-function findLongestWordLength(str) {
-  const arr = str.split(" ");
-  console.log(arr[4].length);
-  let count = arr[0].length;
+// function findLongestWordLength(str) {
+//   const arr = str.split(" ");
+//   console.log(arr[4].length);
+//   let count = arr[0].length;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i].length > count) {
+//       count = arr[i].length;
+//     }
+//   }
+//   return count;
+// }
+
+// findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+function largestOfFour(arr) {
+  const nums = [];
+  let largest = 0;
+  // First loop to iterate over the arrays
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i].length > count) {
-      count = arr[i].length;
+    // Second loop to iterate over the sub arrays
+    largest = arr[i][0];
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > largest) {
+        largest = arr[i][j];
+      }
     }
+    nums.push(largest);
   }
-  return count;
+  return nums;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
